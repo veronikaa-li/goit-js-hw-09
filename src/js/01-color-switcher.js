2aeb360944e6ea87e7cce943b469525d0a4b7832
veronikaa-li <li.veronika92@gmail.com>
2023-03-18 18:28:31 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
 
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  //Отримуємо посилання на кнопки та body//
+  const body = document.querySelector('body');
+  const startBtn = document.querySelector('[data-start]');
+  const stopBtn = document.querySelector('[data-stop]');
+ 
+  //ідентифікатор зміни кольору//
+  let intervalId;
+  //При Start-запускаємо зміну кольорів 1сек//
+  startBtn.addEventListener('click', () => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    intervalId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+  //при натисканні на Stop зупиняємо зміну кольорів//
+  stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+  });
+  
+
+
