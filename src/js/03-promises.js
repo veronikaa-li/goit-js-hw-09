bd7a934c457a2a12805bdb5a67d7476f50fd04ef
veronikaa-li <li.veronika92@gmail.com>
2023-03-21 22:17:10 +0200

    correction

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -17,25 +17,19 @@ function onSubmit(e) {
   let step = Number(refs.step.value);
   let amount = Number(refs.amount.value);
 
-  let position = 0;
-  let promiseDelay = 0;
-
   for (let i = 1; i <= amount; i += 1) {
-    position += i;
-    promiseDelay = delay;
-    promiseDelay += step;
-
-    createPromise(position, promiseDelay)
+    createPromise(i, delay)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
-     }
+    delay += step;
+  }
 }
 
-function createPromise(position, delay) {
+  function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
   const shouldResolve = Math.random() > 0.3;
   setTimeout(() => {
@@ -49,6 +43,9 @@ function createPromise(position, delay) {
 }, delay);
   });
 }
+
+
+
 
 
 // function createPromise(position, delay) {
